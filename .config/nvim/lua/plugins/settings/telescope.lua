0634d36b90cd05fee8007917fd757974d3c6474f
strongleong <nikita_chul@mail.ru>
2022-05-17 10:19:42 +1100

    [Neovim] Fixed `Telescope` background color

--- .config/nvim/lua/plugins/settings/telescope.lua
+++ .config/nvim/lua/plugins/settings/telescope.lua
@@ -4,5 +4,16 @@ if not status_ok then
 	return
 end
 
+vim.api.nvim_set_hl(0, "TelescopeNormal",        {bg="none", fg="#C8D0E0"})
+vim.api.nvim_set_hl(0, "TelescopePreviewNormal", {bg="none", fg="#C8D0E0"})
+vim.api.nvim_set_hl(0, "TelescopePromptNormal",  {bg="none", fg="#C8D0E0"})
+vim.api.nvim_set_hl(0, "TelescopeResultsNormal", {bg="none", fg="#C8D0E0"})
+
+vim.api.nvim_set_hl(0, "TelescopeBorder",        {bg="none", fg="#81A1C1"})
+vim.api.nvim_set_hl(0, "TelescopePromptBorder",  {bg="none", fg="#81A1C1"})
+vim.api.nvim_set_hl(0, "TelescopeResultsBorder", {bg="none", fg="#81A1C1"})
+vim.api.nvim_set_hl(0, "TelescopePreviewBorder", {bg="none", fg="#81A1C1"})
+
 telescope.load_extension('fzf')
 telescope.load_extension('media_files')
+
