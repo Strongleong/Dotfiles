fc0cf6ab4056f95ad43f6d70c298e3c4b918e76d
strongleong <nikita_chul@mail.ru>
2022-08-21 14:56:48 +1100

    [zsh] Showing vi mode in prompt

--- .config/zsh/prompt.sh
+++ .config/zsh/prompt.sh
@@ -25,5 +25,21 @@ zstyle ':vcs_info:*' check-for-changes true
 zstyle ':vcs_info:git:*' formats " %{$fg[blue]%}(%{$fg[red]%}%m%u%c%{$fg[yellow]%}%{$fg[magenta]%} %b%{$fg[blue]%})"
 
 # format our main prompt for hostname current folder, and permissions.
-PROMPT="%B%{$fg[cyan]%}%c\$vcs_info_msg_0_ %{$fg[blue]%}> %{$reset_color%}"
+BASE_PROMPT="%B%{$fg[cyan]%}%c\$vcs_info_msg_0_ %{$fg[blue]%}> %{$reset_color%}"
+PROMPT=$BASE_PROMPT
 RPROMPT="%{$reset_color%}%(?.. %{$fg[red]%}%B ERR )%{$fg[blue]%}%*%{$reset_color%}"
+
+
+function zle-line-init zle-keymap-select {
+    RPS1="${${KEYMAP/vicmd/N}/(main|viins)/I}"
+    PROMPT="%B%{$fg[cyan]%}[$RPS1] $BASE_PROMPT"
+    case $KEYMAP in
+        vicmd) echo -ne '\e[1 q';;      # block
+        viins|main) echo -ne '\e[5 q';; # beam
+    esac
+    RPROMPT="%{$reset_color%}%(?.. %{$fg[red]%}%B ERR )%{$fg[blue]%}%*%{$reset_color%}"
+    zle reset-prompt
+}
+
+zle -N zle-line-init
+zle -N zle-keymap-select
