5a03df6f429ab7ea04a3a541ca6c7fa2e0d73285
strongleong <nikita_chul@mail.ru>
2022-12-05 10:42:54 +1100

    [Qtile] Refactored colors

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -13,6 +13,7 @@ terminal = guess_terminal()
 browser = "firefox"
 gap_size = 15
 editor = "neovide --multigrid"
+colorscheme = "onedark"
 
 
 def set_wallpaper(qtile=None):
@@ -37,6 +38,11 @@ def spawn_float_term(qtile, *argv):
         else:
             command.append(argument)
     return subprocess.Popen(command)
+
+
+@lazy.function
+def yay_float():
+    spawn_float_term(['yay'])
 
 
 keys = [
@@ -91,10 +97,17 @@ keys = [
     Key([], "Print", lazy.spawn('flameshot gui'), desc="Spawns flameshow"),
     Key([], "XF86MonBrightnessUp", lazy.spawn('light -A 10'), desc="Decrease backlight"),
     Key([], "XF86MonBrightnessDown", lazy.spawn('light -U 10'), desc="Inccrease backlight"),
-    Key(['control', 'shift'], "l", lazy.spawn("dm-tool lock"), desc="Lock screen"),
+    Key(['control', 'shift'], "l", lazy.spawn("my-lock"), desc="Lock screen"),
     Key([mod, 'control'], 'h', lazy.to_screen(0), desc="Move to screen 1"),
     Key([mod, 'control'], 'l', lazy.to_screen(1), desc="Move to screen 2"),
+    Key([mod], 'y', yay_float(), desc="Update packages in float term"),
     Key([mod], 'b', lazy.hide_show_bar(), desc="Toggle bar"),
+    Key(
+        [mod, 'shift'],
+        'f',
+        spawn_float_term(['tmux', 'new-session', '-A', '-s', 'float']),
+        desc="Spawn floating term"
+    ),
     Key(
         [mod],
         "space",
@@ -148,23 +161,59 @@ layouts = [
 
 
 def init_colors():
-    return [["#2e3440", "#2e3440"],  # color 0  dark grayish blue
-            ["#2e3440", "#2e3440"],  # color 1  dark grayish blue
-            ["#3b4252", "#3b4252"],  # color 2  very dark grayish blue
-            ["#434c5e", "#434c5e"],  # color 3  very dark grayish blue
-            ["#4c566a", "#4c566a"],  # color 4  very dark grayish blue
-            ["#d8dee9", "#d8dee9"],  # color 5  grayish blue
-            ["#e5e9f0", "#e5e9f0"],  # color 6  light grayish blue
-            ["#eceff4", "#eceff4"],  # color 7  light grayish blue
-            ["#8fbcbb", "#8fbcbb"],  # color 8  grayish cyan
-            ["#88c0d0", "#88c0d0"],  # color 9  desaturated cyan
-            ["#81a1c1", "#81a1c1"],  # color 10 desaturated blue
-            ["#5e81ac", "#5e81ac"],  # color 11 dark moderate blue
-            ["#bf616a", "#bf616a"],  # color 12 slightly desaturated red
-            ["#d08770", "#d08770"],  # color 13 desaturated red
-            ["#ebcb8b", "#ebcb8b"],  # color 14 soft orange
-            ["#a3be8c", "#a3be8c"],  # color 15 desaturated green
-            ["#b48ead", "#b48ead"]]  # color 16 grayish magenta
+    colors = {
+        'test': {
+            'dark-bg',
+        },
+        'onenord': {
+            # ["#2e3440", "#2e3440"],  # color 0  dark grayish blue
+            'dark-bg': ["#2e3440", "#2e3440"],  # color 1  dark grayish blue
+            # ["#3b4252", "#3b4252"],  # color 2  very dark grayish blue
+            # ["#434c5e", "#434c5e"],  # color 3  very dark grayish blue
+            # ["#4c566a", "#4c566a"],  # color 4  very dark grayish blue
+            'fg': ["#d8dee9", "#d8dee9"],  # color 5  grayish blue
+            'fg-lighter': ["#e5e9f0", "#e5e9f0"],  # color 6  light grayish blue
+            # ["#eceff4", "#eceff4"],  # color 7  light grayish blue
+            'primary': ["#8fbcbb", "#8fbcbb"],  # color 8  grayish cyan
+            # ["#88c0d0", "#88c0d0"],  # color 9  desaturated cyan
+            'notify': ["#81a1c1", "#81a1c1"],  # color 10 desaturated blue
+            # ["#5e81ac", "#5e81ac"],  # color 11 dark moderate blue
+            # ["#bf616a", "#bf616a"],  # color 12 slightly desaturated red
+            # ["#d08770", "#d08770"],  # color 13 desaturated red
+            # ["#ebcb8b", "#ebcb8b"],  # color 14 soft orange
+            # ["#a3be8c", "#a3be8c"],  # color 15 desaturated green
+            'bg-inactive':  ["#b48ead", "#b48ead"]
+        },
+        'onedark': {
+            # ["#21283b", "#21283b"],
+            'dark-bg': ["#1a212e", "#1a212e"],
+            # ["#283347", "#283347"],
+            # ["#2a324a", "#2a324a"],
+            # ["#141b24", "#141b24"],
+            'fg': ["#54b0fd", "#54b0fd"],
+            # ["#f2cc81", "#f2cc81"],
+            # ["#93a4c3", "#93a4c3"],
+            'primary': ["#c75ae8", "#c75ae8"],
+            # 'notify': ["#8bcd5b", "#8bcd5b"],
+            # ["#dd9046", "#dd9046"],
+            # ["#41a7fc", "#41a7fc"],
+            'notice': ["#efbd5d", "#efbd5d"],
+            # ["#34bfd0", "#34bfd0"],
+            # ["#f65866", "#f65866"],
+            # ["#455574", "#455574"],
+            # ["#6c7d9c", "#6c7d9c"],
+            'bg-inactive': ["#1b6a73", "#1b6a73"],
+            # ["#992525", "#992525"],
+            # ["#8f610d", "#8f610d"],
+            'notify': ["#862aa1", "#862aa1"],
+            # ["#27341c", "#27341c"],
+            # ["#331c1e", "#331c1e"],
+            # ["#102b40", "#102b40"],
+            # ["#1c4a6e", "#1c4a6e"]
+        }
+    }
+
+    return colors[colorscheme]
 
 
 colors = init_colors()
@@ -175,8 +224,8 @@ widget_defaults = dict(
     font='RobotoMono Nerd Font',
     fontsize=12,
     padding=3,
-    background=colors[1],
-    foreground=colors[5]
+    background=colors['dark-bg'],
+    foreground=colors['fg']
 )
 extension_defaults = widget_defaults.copy()
 
@@ -184,73 +233,73 @@ extension_defaults = widget_defaults.copy()
 def init_widgets_list(monitor):
     widget_list_left = [
         widget.Sep(
-            background=colors[1],  # 2e3440
-            foreground=colors[5],  # d8dee9
+            background=colors['dark-bg'],  # 2e3440
+            foreground=colors['fg'],  # d8dee9
             linewidth=1,
             padding=10
         ),
         widget.Image(
             filename="~/.config/qtile/icons/qtilelogo.png",
             iconsize=8,
-            background=colors[1],
+            background=colors['dark-bg'],
             mouse_callbacks={'Button1': lambda: qtile.cmd_spawn('rofi -show drun')}
         ),
         widget.Sep(
-            background=colors[1],
-            foreground=colors[5],
+            background=colors['dark-bg'],
+            foreground=colors['fg'],
             linewidth=1,
             padding=10
         ),
         widget.GroupBox(
-            active=colors[6],
+            active=colors['fg'],
             borderwidth=2,
             disable_drag=True,
             font='RobotoMono Nerd Font',
             fontsize=14,
             hide_unused=False,
             highlight_method='line',
-            inactive=colors[16],
+            inactive=colors['bg-inactive'],
             margin_x=0,
             margin_y=3,
             padding_x=5,
             padding_y=8,
             rounded=False,
-            this_current_screen_border=colors[14],  # ebcb8b
+            this_current_screen_border=colors['notice'],
             urgent_alert_method='line'
         ),
         widget.Sep(
-            background=colors[1],
-            foreground=colors[5],
+            background=colors['dark-bg'],
+            foreground=colors['fg'],
             linewidth=1,
             padding=10
         ),
         widget.CurrentLayoutIcon(
-            background=colors[1],
+            background=colors['dark-bg'],
             custom_icon_paths=[os.path.expanduser("~/.config/qtile/icons")],
-            foreground=colors[6],  # e5e9f0
+            foreground=colors['fg'],  # e5e9f0
             padding=0,
             scale=0.65
         ),
         widget.CurrentLayout(
-            background=colors[1],
+            background=colors['dark-bg'],
             font='RobotoMono Bold',
-            foreground=colors[6]
+            foreground=colors['fg']
         ),
         widget.Sep(
-            background=colors[1],
-            foreground=colors[5],
+            background=colors['dark-bg'],
+            foreground=colors['fg'],
             linewidth=1,
             padding=10
         ),
         widget.Prompt(
-            background=colors[1],
+            background=colors['dark-bg'],
             font='RobotoMono',
             fontsize=12,
-            foreground=colors[6]
+            foreground=colors['fg']
         ),
         widget.WindowTabs(
-            background=colors[1],
-            foreground=colors[6],
+            background=colors['dark-bg'],
+            foreground=colors['fg'],
             font='RobotoMono',
             fontsize=12,
             max_chars=60
@@ -263,51 +312,51 @@ def init_widgets_list(monitor):
 
     widget_list_right = [
         widget.Sep(
-            background=colors[1],
-            foreground=colors[5],
+            background=colors['dark-bg'],
+            foreground=colors['fg'],
             linewidth=1,
             padding=10
         ),
         widget.TextBox(
-            background=colors[1],
+            background=colors['dark-bg'],
             font='RobotoMono Nerd Font',
             fontsize=14,
-            foreground=colors[6],
+            foreground=colors['fg'],
             padding=0,
             text=' '
         ),
         widget.KeyboardLayout(
-            background=colors[1],
+            background=colors['dark-bg'],
             font='RobotoMono',
             configured_keyboard=['ru', 'us'],
             fontsize=12,
-            foreground=colors[6]
+            foreground=colors['fg']
         ),
         widget.CapsNumLockIndicator(
-            background=colors[1],
+            background=colors['dark-bg'],
             font='RobotoMono',
             fontsize=12,
-            foreground=colors[6]
+            foreground=colors['fg']
         ),
         widget.Sep(
-            background=colors[1],
-            foreground=colors[5],
+            background=colors['dark-bg'],
+            foreground=colors['fg'],
             linewidth=1,
             padding=10
         ),
         widget.TextBox(
-            background=colors[1],
+            background=colors['dark-bg'],
             font='RobotoMono Nerd Font',
             fontsize=14,
-            foreground=colors[6],
+            foreground=colors['fg'],
             padding=0,
             text=' '
         ),
         widget.Clock(
-            background=colors[1],
+            background=colors['dark-bg'],
             font='RobotoMono',
             fontsize=12,
-            foreground=colors[6],
+            foreground=colors['fg'],
             format='%a %d, (%B) %H:%M:%S '
         ),
     ]
@@ -315,39 +364,39 @@ def init_widgets_list(monitor):
     if monitor == 1:
         widget_list_unique = [
             widget.Systray(
-                background=colors[1],
+                background=colors['dark-bg'],
                 icon_size=20,
                 padding=4
             ),
             widget.Sep(
-                background=colors[1],
-                foreground=colors[5],
+                background=colors['dark-bg'],
+                foreground=colors['fg'],
                 linewidth=1,
                 padding=10
             ),
             widget.TextBox(
-                background=colors[1],
+                background=colors['dark-bg'],
                 font='RobotoMono Nerd Font',
                 fontsize=14,
-                foreground=colors[6],
+                foreground=colors['fg'],
                 padding=0,
                 text=' '
             ),
             widget.Backlight(
-                background=colors[1],
+                background=colors['dark-bg'],
                 font='RobotoMono',
                 fontsize=12,
                 backlight_name='intel_backlight',
-                foreground=colors[6],
+                foreground=colors['fg'],
                 update_interval=0.2
             ),
         ]
     elif monitor == 2:
         widget_list_unique = [
             widget.CheckUpdates(
-                background=colors[1],
-                colour_have_updates=colors[10],
-                foreground=colors[6],
+                background=colors['dark-bg'],
+                colour_have_updates=colors['notify'],
+                foreground=colors['fg'],
                 fontsize=12,
                 linewidth=1,
                 padding=10,
@@ -356,7 +405,7 @@ def init_widgets_list(monitor):
                 distro='Arch',
                 custom_command='checkupdates; yay -Qua',
                 mouse_callbacks={
-                    'Button1': spawn_float_term(['yay', '-S', 'u', 'y', '--noconfirm'])
+                    'Button1': yay_float()
                 },
             ),
         ]
@@ -368,59 +417,60 @@ def init_bottom_widgets_list():
     return [
         widget.Spacer(),
         widget.Sep(
-            background=colors[1],
-            foreground=colors[5],
+            background=colors['dark-bg'],
+            foreground=colors['fg'],
             linewidth=1,
             padding=10
         ),
         widget.TextBox(
-            background=colors[1],
+            background=colors['dark-bg'],
             font='RobotoMono Nerd Font',
             fontsize=14,
-            foreground=colors[6],
+            foreground=colors['fg'],
             padding=0,
             text=' '
         ),
         widget.ThermalSensor(
-            background=colors[1],
+            background=colors['dark-bg'],
             font='RobotoMono',
             fontsize=12,
-            foreground=colors[6],
+            foreground=colors['fg'],
             tag_sensor='Package id 0',
             update_interval=0.2
         ),
         widget.Sep(
-            background=colors[1],
-            foreground=colors[5],
+            background=colors['dark-bg'],
+            foreground=colors['fg'],
             linewidth=1,
             padding=10
         ),
         widget.TextBox(
-            background=colors[1],
+            background=colors['dark-bg'],
             font='RobotoMono Nerd Font',
             fontsize=14,
             mouse_callbacks={
                 'Button1': lambda: qtile.cmd_spawn('alacritty -e htop -s PERCENT_MEM')
             },
-            foreground=colors[6],
+            foreground=colors['fg'],
             padding=0,
             text=' '
         ),
         widget.Memory(
-            background=colors[1],
+            background=colors['dark-bg'],
             font='RobotoMono',
             mouse_callbacks={
                 'Button1': lambda: qtile.cmd_spawn('alacritty -e htop -s PERCENT_MEM')
             },
             fontsize=12,
             format='{MemUsed: .2f}{mm}/{MemTotal: .2f}{mm}',
-            foreground=colors[6],
+            foreground=colors['fg'],
+            graph_color=colors['primary'],
             measure_mem='G'
         ),
         widget.MemoryGraph(
             type='line',
             line_width=1,
-            graph_color=colors[8],
+            graph_color=colors['primary'],
             mouse_callbacks={
                 'Button1': lambda: qtile.cmd_spawn('alacritty -e htop -s PERCENT_MEM')
             },
@@ -428,27 +478,27 @@ def init_bottom_widgets_list():
             samples=1000
         ),
         widget.Sep(
-            background=colors[1],
-            foreground=colors[5],
+            background=colors['dark-bg'],
+            foreground=colors['fg'],
             linewidth=1,
             padding=10
         ),
         widget.TextBox(
-            background=colors[1],
+            background=colors['dark-bg'],
             font='RobotoMono Nerd Font',
             fontsize=14,
             mouse_callbacks={
                 'Button1': lambda: qtile.cmd_spawn('alacritty -e htop -s PERCENT_CPU')
             },
-            foreground=colors[6],
+            foreground=colors['fg'],
             padding=0,
             text=' '
         ),
         widget.CPU(
-            background=colors[1],
+            background=colors['dark-bg'],
             font='RobotoMono',
             fontsize=12,
-            foreground=colors[6],
+            foreground=colors['fg'],
             mouse_callbacks={
                 'Button1': lambda: qtile.cmd_spawn('alacritty -e htop -s PERCENT_CPU')
             },
@@ -458,7 +508,7 @@ def init_bottom_widgets_list():
         widget.CPUGraph(
             type='line',
             line_width=1,
-            graph_color=colors[5],
+            graph_color=colors['fg'],
             mouse_callbacks={
                 'Button1': lambda: qtile.cmd_spawn('alacritty -e htop -s PERCENT_CPU')
             },
@@ -466,36 +516,36 @@ def init_bottom_widgets_list():
             samples=1000
         ),
         widget.Sep(
-            background=colors[1],
-            foreground=colors[5],
+            background=colors['dark-bg'],
+            foreground=colors['fg'],
             linewidth=1,
             padding=10
         ),
         widget.TextBox(
-            background=colors[1],
+            background=colors['dark-bg'],
             font='RobotoMono Nerd Font',
             fontsize=14,
-            foreground=colors[6],
+            foreground=colors['fg'],
             padding=0,
             text='  '
         ),
         widget.Net(
-            background=colors[1],
+            background=colors['dark-bg'],
             font='RobotoMono',
             fontsize=12,
-            foreground=colors[5],
+            foreground=colors['fg'],
             format='{interface}: {down} ↓ ',
             interface='all',
             padding=0
         ),
         widget.Sep(
-            background=colors[1],
-            foreground=colors[5],
+            background=colors['dark-bg'],
+            foreground=colors['fg'],
             linewidth=1,
             padding=10
         ),
         widget.Battery(
-            background=colors[1],
+            background=colors['dark-bg'],
             charge_char='',
             discharge_char='',
             empty_char='',
@@ -503,12 +553,12 @@ def init_bottom_widgets_list():
             font='RobotoMono',
             format='{char}  {percent:2.0%}',
             fontsize=12,
-            foreground=colors[5],
+            foreground=colors['fg'],
             padding=0
         ),
         widget.Sep(
-            background=colors[1],
-            foreground=colors[5],
+            background=colors['dark-bg'],
+            foreground=colors['fg'],
             linewidth=1,
             padding=10
         ),
