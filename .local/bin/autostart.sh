fadccc42c956929415d44204901889c157b144d0
Strongleong <nikita_chul@mail.ru>
2023-10-08 00:08:00 +0000

    [autostart.sh] removed broken logging

--- .local/bin/autostart.sh
+++ .local/bin/autostart.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
+if [ -f /tmp/dwm_started ]; then
+  exit
+fi
+
+touch /tmp/dwm_started
+
 # /usr/bin/xrandr --auto --output HDMI1 --mode 1920x1080 --right-of eDP1 &#!/bin/bash
-/usr/bin/setxkbmap -layout "us,ru" -option "grp:alt_shift_toggle" &
+/usr/bin/setxkbmap -layout 'us,ru' -option 'grp:alt_shift_toggle' &
 /usr/bin/gentoo-pipewire-launcher &
-/usr/bin/picom -f --animations --experimental-backends -b --animation-for-open-window zoom &
+/usr/bin/picom -b &
 /usr/local/bin/dwmblocks &
 /usr/bin/volumeicon &
-/usr/bin/bgchd -bcknd feh -dir Images/wallpapers/ -intv 3m
+/usr/bin/bgchd -bcknd feh -dir Images/wallpapers/ -intv 15m &
 /usr/bin/bgctl next &
-/bin/sh -c "/usr/bin/sxhkd &" &
+/bin/sh -c '/usr/bin/sxhkd &' &
 /usr/bin/unclutter &
-
